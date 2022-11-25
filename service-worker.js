3b3a7d62b79a548348d4581f2693595459eed986
Dan Cecile (bot) <dancecile+bot@gmail.com>
2022-11-25 17:35:14 +0000

    Compiled from c28b042 :robot:

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,17 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+// Set up a no-op service worker, to allow uninstalling
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
 
-importScripts(
-  "/precache-manifest.5f1feb7e7222c4f9ec77dd42eed7ed0e.js"
-);
-
-workbox.core.skipWaiting();
-
-workbox.core.clientsClaim();
-
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+self.addEventListener('activate', () => {
+  self.clients
+    .matchAll({
+      type: 'window',
+    })
+    .then((windowClients) => {
+      windowClients.forEach((windowClient) => {
+        windowClient.navigate(windowClient.url)
+      })
+    })
+})
